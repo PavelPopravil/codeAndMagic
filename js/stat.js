251620f10b3f8c8fd57a92968862eb62a82d9180
Shishic Pavel <shishic@db.by>
2018-03-20 15:51:38 +0300

    init createGist func

--- js/stat.js
+++ js/stat.js
@@ -9,16 +9,21 @@ window.getMaxValue = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-    console.log(1);
+    //block vars
     var blockX = 100;
     var blockY = 10;
-    var blockH = 420;
-    var blockW = 270;
+    var blockH = 270;
+    var blockW = 420;
     var horGutter = 20;
     var verticalGutter = 40;
     var fz = 16;
+    // gist vars
     var gistH = 150;
     var gistW = blockW - (horGutter * 2);
+    var gistItemW = 40;
+    var gistItemGutter = 50;
+    var gistItemColor = 'rgba(255, 0, 0, 1)';
+    var gistItemColor2 = 'blue';
 
     (function printBlock() {
         ctx.fillStyle = '#ffffff';
@@ -29,10 +34,26 @@ window.renderStatistics = function (ctx, names, times) {
         ctx.fillText("Список результатов:",blockX + horGutter, blockY + verticalGutter + fz);
     })();
 
-    (function printGist() {
-        ctx.fillStyle = '#000000';
-        ctx.fillRect(blockX + 20, blockY + (verticalGutter * 2) + fz, gistW, gistH);
-    })();
 
+    function createGist(xPos) {
+        ctx.fillStyle = gistItemColor2;
+        ctx.fillRect(xPos + horGutter, blockY + verticalGutter + (fz*2), gistItemW, gistH);
+    }
+
+    for (var i = 0; i < times.length; i++) {
+
+        // var max getMaxValue(times) = 100;
+
+        createGist(blockX);
+        blockX += gistItemGutter + gistItemW;
+    }
+
+
+    // function setProprtion() {
+    //
+    // }
+
+    // console.log(times);
+    // console.log(getMaxValue(times));
     window.getMaxValue(times);
 };
