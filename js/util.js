ac7584f3d9b9981a977cd0a7ea81aec9cd8bf189
Shishic <pasukmc@gmail.com>
2018-06-19 19:21:02 +0300

    switch json to js

--- js/util.js
+++ js/util.js
@@ -60,6 +60,6 @@
         document.body.append(newScirpt);
     }
 
-    addScript('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
+    addScript('https://raw.com/PavelPopravil/codeAndMagic/master/js/data?callback=dataCallback')
 
 })();
